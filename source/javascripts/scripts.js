8ac8084281fec527c021370625bc4b8d2be4ed7a
Alex Lauderdale <alauderdale@mac.com>
2014-08-24 23:57:28 -0600

    cards

--- source/javascripts/scripts.js
+++ source/javascripts/scripts.js
@@ -1,38 +1,5 @@
 $(document).ready(function(){
 
-	 //fancybox
-	$('.fancybox').fancybox();
-	
-	$('.fb-video').fancybox({
-        padding: 0,
-        helpers: {
-            media: {}
-        }
-   });
 
-	$('.tooltip-up').tooltip();
-
-	///responsive tables!!!
-	$('table').footable();
-
-	$('#logo-slider .item:first-child').addClass('active');
-  $('#testimonial-slider .item:first-child').addClass('active');
-
-	$('.carousel').carousel({
-  	interval: 4000
-	})
-
-	$(window).scroll(function(){
-
-    var divOffset = $('#mainContents').offset();
-    if(window.scrollY > divOffset.top ){
-      $('#hidden-menu').fadeIn(400);
-    }
-
-   if(window.scrollY < divOffset.top){
-      $('#hidden-menu').fadeOut(90);
-    }
-
-  });
 
 });
